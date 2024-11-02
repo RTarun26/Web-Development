79dd69e77bd8d1590f69fa697fbe665993263e54
Tarun <rtarun8055@gmail.com>
2024-11-02 11:08:45 +0530

    main

--- 46/script.js
+++ 46/script.js
@@ -1,1 +1,13 @@
 alert("Hello world");
+
+console.log("Code is running...")
+
+var a= prompt("enter your number")
+
+var b= confirm("Are you sure you want to leave this site")
+if(b){
+    console.log("leaving")
+}
+else{
+    console.log("not leaving")
+}
